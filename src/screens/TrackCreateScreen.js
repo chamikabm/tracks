61720be307b9dce592e8a879c924a8b7deaae667
chamikabm <chamika@bitsmedia.com>
2019-12-08 17:16:54 +0800

    Added test code for expo location change mock.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import '../test/mockLocation';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { Text } from 'react-native-elements';
 import Map from '../components/Map';
+import {
+  requestPermissionsAsync, watchPositionAsync, Accuracy,
+} from 'expo-location';
 
 const TrackCreateScreen = () => {
+  const [ error, setError] = useState('');
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10,
+      }, (location) => {
+        console.log(location);
+      });
+    } catch (err) {
+      console.log(`Something went wrong while requesting permissions. ${err}`);
+      setError(err);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
 
   return (
       <SafeAreaView forceInset={{ top: 'always' }}>
         <Text h3>TrackCreate Screen</Text>
         <Map/>
+        { error ? <Text>Please enable location services.</Text> : null}
       </SafeAreaView>
   );
 };
